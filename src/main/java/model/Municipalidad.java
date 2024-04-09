681ccf68212cbb64b8f174e7ef00914d36e79dd8
millrnv <n.aguilar02@ufromail.cl>
2024-04-09 16:49:26 -0400

    clase main

--- src/main/java/model/Municipalidad.java
+++ src/main/java/model/Municipalidad.java
@@ -3,12 +3,27 @@ package model;
 import java.util.ArrayList;
 
 public class Municipalidad {
+
+	private String nombre;
 	public GestionMunicipal GestionMunicipal;
+
+	public Municipalidad(String nombre){
+		this.nombre = nombre;
+
+	}
 	private ArrayList<Permiso> permisos = new ArrayList<Permiso>();
 	private ArrayList<Ciudadano> ciudadanos = new ArrayList<Ciudadano>();
 
 	public model.GestionMunicipal getGestionMunicipal() {
 		return GestionMunicipal;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 
 	public void setGestionMunicipal(model.GestionMunicipal gestionMunicipal) {
